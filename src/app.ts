bc23ebffe8bf6680827cc26c7c8a3463799a0659
Irina Vinitskaia <kreneva.i@yandex.ru>
2024-01-15 20:13:27 +0300

    fix: добавлена библиотека hemlet и тексты ошибок к обязательным полям схем

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,12 @@ import 'dotenv/config';
 import cardRouter from './routes/cards';
 import userRouter from './routes/user';
 
+const helmet = require('helmet');
+
 const { PORT = 3003, MONGO_URL } = process.env;
 
 const app = express();
-
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- src/models/card.ts
+++ src/models/card.ts
@@ -1,5 +1,7 @@
 import mongoose, { ObjectId, Schema } from 'mongoose';
 
+const validator = require('validator');
+
 interface ICard {
   name: string;
   link: string;
@@ -11,13 +13,17 @@ interface ICard {
 const cardSchema = new mongoose.Schema<ICard>({
   name: {
     type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
+    required: [true, 'Поле "name" должно быть заполнено'],
+    minlength: [2, 'Минимальная длина поля "name" - 2'],
+    maxlength: [30, 'Максимальная длина поля "name" - 30'],
   },
   link: {
     type: String,
     required: true,
+    validate: {
+      validator: (value: String) => (validator.isURL(value, { protocols: ['http', 'https', 'ftp'], require_tld: true, require_protocol: true })),
+      message: 'Введите корректную ссылку на картинку',
+    },
   },
   owner: {
     type: Schema.Types.ObjectId,
